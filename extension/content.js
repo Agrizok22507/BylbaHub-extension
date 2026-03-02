f10e7107bb5c5981d2b2f5b486c144ef26278cfe
Capybara <KapibarovKonstantin@gmail.com>
2026-03-02 21:36:28 +0400

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,33 @@
-import('https://bylbahub.onrender.com/static/main.js')
-    .then(() => console.log('BylbaHub successfully injected'))
-    .catch(error => console.error('Error: ', error));
+(function() {
+    console.log('BylbaHub is loading');
+    
+    const script = document.createElement('script');
+    script.src = 'https://bylbahub.onrender.com/static/main.js';
+    
+    script.onload = () => {
+        console.log('BylbaHub successfully injected');
+    };
+    
+    script.onerror = (error) => {
+        console.error('Failed inject: ', error);
+        fallbackLoad();
+    };
+    
+    document.documentElement.appendChild(script);
+    
+    function fallbackLoad() {
+        console.log('Another inject');
+        
+        fetch('https://bylbahub.onrender.com/static/main.js')
+            .then(response => response.text())
+            .then(code => {
+                const fallbackScript = document.createElement('script');
+                fallbackScript.textContent = code;
+                document.documentElement.appendChild(fallbackScript);
+                console.log('BylbaHub successfully injected');
+            })
+            .catch(err => {
+                console.error('Failed inject: ', err);
+            });
+    }
+})();
